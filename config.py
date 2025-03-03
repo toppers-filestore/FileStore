89c7636680c2814079eade893b9b5481b03f9466
toppers-filestore <nihalfilestore@gmail.com>
2025-03-04 01:14:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ from logging.handlers import RotatingFileHandler
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7944545219:")
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "22182189"))
+API_ID = int(os.environ.get("API_ID", "22182189"))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 #Your db channel Id
